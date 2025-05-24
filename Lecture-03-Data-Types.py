214bd2324e638407efa4858192064c034081fa02
urduitacademy <59315697+urduitacademy@users.noreply.github.com>
2025-05-24 14:33:59 +0100

    Create Lecture-03-Data-Types.py

---
+++ Lecture-03-Data-Types.py
@@ -0,0 +1,42 @@
+# 📘 Lecture 3 – Python Data Types & Data Structures
+# Urdu IT Academy | Python for Beginners
+
+# Integer
+a = 5
+print(a)
+print("Type of a:", type(a))
+
+# Float
+a = 2.5
+print(a)
+print("Type of a:", type(a))
+
+# Complex Number
+a = 20j
+print(a)
+print("Type of a:", type(a))
+
+# String
+a = "hello"
+print(a)
+print("Type of a:", type(a))
+
+# List
+a = ['apple', 'banana', 'cherry']
+print(a)
+print("Type of a:", type(a))
+
+# Tuple
+a = (10, 20, 30)
+print(a)
+print("Type of a:", type(a))
+
+# Boolean
+a = True
+print(a)
+print("Type of a:", type(a))
+
+# Dictionary
+a = {'name': 'Ali', 'age': 25}
+print(a)
+print("Type of a:", type(a))
